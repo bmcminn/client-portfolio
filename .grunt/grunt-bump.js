57baabf0ab4ec76375187b1e7722825494ee0224
bmcminn <labs@gbox.name>
2015-04-22 17:26:30 -0500

    adding npm config and grunt packages to build out local build tooling

---
+++ .grunt/grunt-bump.js
@@ -0,0 +1,15 @@
+/* jshint laxbreak:true, laxcomma:true */
+/* global module, require, __dirname */
+
+module.exports = function(grunt) {
+
+  'use strict';
+
+  var _     = require('lodash')
+    , chalk = require('chalk')
+    , path  = require('path')
+    ;
+
+
+
+};
