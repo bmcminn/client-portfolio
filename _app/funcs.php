46bc013ce9682e368219ac847c6fb23bbb8016e5
bmcminn <labs@gbox.name>
2015-05-08 01:01:34 -0500

    refactoring login route to be more functional, cleaning up styles, modifying session state routines

--- _app/funcs.php
+++ _app/funcs.php
@@ -22,29 +22,171 @@
   }
 
 
+
+  /**
+   * Runs the user login routine
+   * @return null
+   */
+  function loginUser() {
+
+    global $appModel, $handlebars, $db, $passHash;
+
+    $eMessage = [];
+
+    // If the form was submitted
+    // AND we have POST data
+    // TODO: AND the referrer wasn't a foreign (cURL|wget) request
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+
+      // alias our login creds
+      $username = strtolower($_POST['username']); // lower caseing to ensure we don't have duplicate usernames
+      $password = $_POST['password'];
+
+      // check if USERNAME is an empty string
+      if (empty($username)) {
+        $eMessage[] = 'Username is a required field';
+      }
+
+      // check if PASSWORD is an empty string
+      if (empty($password)) {
+        $eMessage[] = 'Password is a required field';
+      }
+
+      // cache the posted USERNAME in our viewmodel to hydrate the field
+      $appModel['loginInfo'] = [
+        'username' => $_POST['username']
+      ];
+
+      // lets test our creds
+      if (!empty($username) && !empty($password)) {
+
+        if (strlen($password) > 72) {
+          $eMessage[] = "Password must be 72 characters or less";
+        } else {
+
+          // Get our user data from the DB
+          $user = $db->users->find(function($document) use ($username, $password, $passHash) {
+            return $document["username"] === $username
+                && $passHash->CheckPassword($password, $document["password"])
+              ;
+          });
+
+          // User is valid, we can log them in :)
+          if ($user->valid()) {
+
+            // alias the current User record
+            $user = $user->current();
+
+            // unset various user data we don't want to update
+            unset($user['_id']);
+            unset($user['password']);
+
+            // update the user timestamp value to now
+            $user = array_replace_recursive(
+              $user
+            , [
+                'timestamp'          => time()
+              , 'lastLogin'          => date('Y-m-d')
+              , 'lastLoginLocation'  => $_SERVER['REMOTE_ADDR']
+              ]);
+
+
+            // update the timestamp for our user record
+            $db->users->update(
+              function($document) use ($username) {
+                return $document['username'] === $username;
+              },
+              $user
+            );
+
+            // set our SESSION user data
+            $_SESSION['user'] = $user;
+
+            // Go to /admin
+            redirect('/admin');
+
+          } else {
+            $eMessage[] = 'The username or password did not match our records';
+          }
+        }
+      }
+    }
+
+    // add error messaging to our view model
+    if (!empty($eMessage)) {
+      $appModel['loginError'] = [
+        'message' => $eMessage
+      ];
+    }
+
+    return null;
+  }
+
+
+
   /**
    * Checks if the 'user' session data has been established
    * @return [type] [description]
    */
-  function checkUserStatus() {
+  function checkUserSession() {
 
-    // the user is logged in
-    if (isset($_SESSION['user'])) {
+    // if the user object is created and the session is still valid
+    if (isset($_SESSION['user']) && (time() - $_SESSION['user']['timestamp'] < SESSION_TIMEOUT)) {
 
-      // the user session has timed out
-      if (!onRoute('login')) {
-        if (time() - $_SESSION['user']['timestamp'] > SESSION_TIMEOUT) {
-          return redirect('/login');
-        }
-      }
-
-      // since we're logged in, we don't need to see the login view
+      // if we go to the login route, take us back to the admin
       if (onRoute('login')) {
         return redirect('/admin');
       }
+
+    // the user is either not logged in or their session timed out
+    } else {
+
+      // if they aren't on the login view, they should be
+      if (!onRoute('login')) {
+        return redirect('/login');
+      }
     }
 
+    return null;
   }
+
+
+
+  /**
+   * [getUserData description]
+   * @return [type] [description]
+   */
+  function getUserData() {
+    global $db, $appModel;
+
+    // get user data
+    $user = $db->users->find(function($document) {
+      return $document["username"] === $_SESSION['user']['username'];
+    });
+
+    if ($user->valid()) {
+      $user = $user->current();
+
+      $_SESSION['user'] = $user;
+
+      // unset data that shouldn't be in here
+      unset($user['_id']);
+      unset($user['lastLoginLocation']);
+      unset($user['password']);
+
+      // merge user data into $appModel
+      $appModel = array_replace_recursive(
+        $appModel,
+        ['user' => $user]
+        );
+
+      // console($user, '$user', 'debug');
+      console($appModel, '$appModel', 'debug');
+    }
+
+    return null;
+  }
+
 
 
   /**
@@ -57,6 +199,7 @@
   }
 
 
+
   /**
    * [setLastRoute description]
    * @param [type] $route [description]
@@ -67,14 +210,17 @@
   }
 
 
+
   /**
    * Logs out the user by destroying the session
    * @return [type] [description]
    */
   function logUserOut() {
+    session_unset();
     session_destroy();
     return redirect('/login');
   }
+
 
 
   /**
@@ -105,6 +251,7 @@
   }
 
 
+
   /**
    * Gets all handlebar partial files from the globally defined VIEWS directory
    * @param  string $viewsPath path to the views directory where partials are kept
@@ -128,6 +275,7 @@
   }
 
 
+
   /**
    * [pathify description]
    * @param  [type] $path [description]
@@ -142,6 +290,7 @@
 
     return $path;
   }
+
 
 
   /**
--- _app/routes.php
+++ _app/routes.php
@@ -4,7 +4,7 @@
   // HOME ROUTE
   //
   map(BASE_URL.'/', function() {
-    checkUserStatus();
+    checkUserSession();
 
     // TODO: Fix this problem with BASE_URL
     // return redirect('/login');
@@ -17,125 +17,22 @@
 
   map(BASE_URL.'/admin', function($db) {
 
+    global $appModel, $handlebars;
+
     setLastRoute(BASE_URL.'/admin');
 
-    checkUserStatus();
+    checkUserSession();
 
-    global $appModel, $handlebars;
+    getUserData();
 
     echo $handlebars->render('admin', $appModel);
   });
 
 
-  //
-  // LOGIN STUFF
-  //
 
-  map(BASE_URL.'/logout', function() {
-    logUserOut();
-  });
+  map(BASE_URL.'/admin/register-client', function($db) {
 
-
-  map(BASE_URL.'/login', function($db, $passHash) {
-
-    checkUserStatus();
-
-    // TODO: move ALL this into a function...
-
-    global $appModel, $handlebars;
-
-
-    $eMessage = [];
-
-    // If the form was submitted
-    // AND we have POST data
-    // TODO: AND the referrer wasn't a foreign (cURL|wget) request
-    if (isset($_POST['username']) && isset($_POST['password'])) {
-
-      // alias our login creds
-      $username = strtolower($_POST['username']); // lower caseing to ensure we don't have duplicate usernames
-      $password = $_POST['password'];
-
-      // check if USERNAME is an empty string
-      if (empty($username)) {
-        $eMessage[] = 'Username is a required field';
-      }
-
-      // check if PASSWORD is an empty string
-      if (empty($password)) {
-        $eMessage[] = 'Password is a required field';
-      }
-
-      // cache the posted USERNAME in our viewmodel to hydrate the field
-      $appModel['loginInfo'] = [
-        'username' => $_POST['username']
-      ];
-
-      // lets test our creds
-      if (!empty($username) && !empty($password)) {
-
-        if (strlen($password) > 72) {
-          $eMessage[] = "Password must be 72 characters or less";
-        } else {
-
-          // Get our user data from the DB
-          $user = $db->users->find(function($document) use ($username, $password, $passHash) {
-            return $document["username"] === $username
-                && $passHash->CheckPassword($password, $document["password"])
-              ;
-          });
-
-          // User is valid, we can log them in :)
-          if ($user->valid()) {
-
-            // alias the current User record
-            $user = $user->current();
-
-            // unset various user data so we don't want to update
-            unset($user['_id']);
-            unset($user['password']);
-
-            // update the user timestamp value to now
-            $user = array_replace_recursive(
-              $user
-            , [
-                'timestamp'          => time()
-              , 'lastLogin'          => date('Y-m-d')
-              , 'lastLoginLocation'  => $_SERVER['REMOTE_ADDR']
-              ]);
-
-
-            // update the timestamp for our user record
-            $db->users->update(
-              function($document) use ($username) {
-                return $document['username'] === $username;
-              },
-              $user
-            );
-
-            // remove any last bits we shouldn't expose to the user
-            unset($user['lastLoginLocation']);
-
-            // set our SESSION data
-            $_SESSION['user'] = $user;
-
-            // Go to /admin
-            redirect('/admin');
-
-          } else {
-            $eMessage[] = 'The username or password did not match our records';
-          }
-        }
-      }
-    }
-
-    // add error messaging to our view model
-    if (!empty($eMessage)) {
-      $appModel['loginError'] = [
-        'message' => $eMessage
-      ];
-    }
-
+    checkUserSession();
 
   // $user = [
   //   "fname"       =>  "Bob"
@@ -154,6 +51,24 @@
   //   ]
   // ];
 
+  });
+
+
+
+  //
+  // LOGIN STUFF
+  //
+
+  map(BASE_URL.'/logout', function() {
+    logUserOut();
+  });
+
+
+  map(BASE_URL.'/login', function($db, $passHash) {
+    global $appModel, $handlebars;
+
+    checkUserSession();
+    loginUser();
 
     echo $handlebars->render('login', $appModel);
   });
